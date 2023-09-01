1385a260887617983684a4254657e20bc006e077
zzj <2461927976@qq.com>
2023-09-01 09:20:58 +0800

    暂存

--- src/app/components/image-slider/image-slider.component.ts
+++ src/app/components/image-slider/image-slider.component.ts
@@ -7,6 +7,7 @@ import {
   ViewChild,
   ViewChildren,
   AfterViewInit,
+  Renderer2,
 } from '@angular/core';
 
 export interface ImageSlider {
@@ -24,13 +25,16 @@ export class ImageSliderComponent implements OnInit, AfterViewInit {
   @ViewChild('imageSlider', { static: true }) imageSlider: ElementRef;
   @ViewChildren('img') imgs: QueryList<ElementRef>;
   @Input() sliders: ImageSlider[] = [];
+  @Input() sliderHeight = '160px';
 
-  constructor() {}
+  constructor(private rd2: Renderer2) {}
   ngAfterViewInit(): void {
-    console.log('222', this.imgs);
+    // this.imgs.forEach((item) => {
+    //   this.rd2.setStyle(item.nativeElement, 'height', '100px');
+    // });
   }
 
   ngOnInit() {
-    console.log('1111', this.imageSlider);
+    // console.log('1111', this.imageSlider);
   }
 }

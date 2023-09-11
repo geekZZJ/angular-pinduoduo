dc9559d9bb13bf75d5820a8f4c28d5f0f80e111b
zzj <2461927976@qq.com>
2023-09-12 00:48:01 +0800

    注解

--- src/app/shared/components/horizontal-grid/horizontal-grid.component.ts
+++ src/app/shared/components/horizontal-grid/horizontal-grid.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Confirmable, Emoji } from '../../decorators';
 
 @Component({
   selector: 'app-horizontal-grid',
@@ -8,6 +9,7 @@ import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 export class HorizontalGridComponent implements OnInit {
   private _username = '';
   @Output() usernameChange = new EventEmitter();
+  @Emoji() result = 'Hello';
 
   constructor() {}
 
@@ -25,5 +27,10 @@ export class HorizontalGridComponent implements OnInit {
 
   handleInput(event: any) {
     this.username = event.target.value;
+  }
+
+  @Confirmable('确定执行？')
+  handleClick() {
+    console.log('3333');
   }
 }

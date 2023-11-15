c8fbaa0ce915f3d1fa565f7ad003d3e44a278cdc
zzj <2461927976@qq.com>
2023-11-15 23:13:32 +0800

    rxjs

--- src/app/home/components/parent/parent.component.ts
+++ src/app/home/components/parent/parent.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { fromEvent } from 'rxjs';
 
 @Component({
   selector: 'app-parent',
   templateUrl: './parent.component.html',
-  styleUrls: ['./parent.component.css']
+  styleUrls: ['./parent.component.css'],
 })
 export class ParentComponent implements OnInit {
+  @ViewChild('inputRef', { static: true }) inputRef: ElementRef;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
+    fromEvent(this.inputRef.nativeElement, 'input').subscribe((ev: any) => {
+      console.log(ev.target.value);
+    });
   }
-
 }
